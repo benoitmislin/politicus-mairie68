7f4982e24c1ecea57e3cd05d412d268d5480998e
b.mislin <b.mislin@thuria.fr>
2023-08-09 11:31:46 +0200

    First commit

---
+++ mairie68/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-red-900">
+        <NuxtLink to="/">Accueil</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ mairie68/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <main>
+        <h1>{{ contents.titre }}</h1>
+        <nuxt-img src="../public/img/image.png" />
+        <ul>
+            <li v-for="item in contents" :key="item.id">
+                {{ item.titre }}
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script setup>
+
+    var titre = "Coucou";
+
+    var contents = [{
+        'titre' : 'Coucou',
+        'texte' : 'Lorem'
+    },
+    {
+        'titre' : 'Zob',
+        'texte' : 'Lorem'
+    }]
+    
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>

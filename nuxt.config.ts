3914e4f92a97f82c3b112295a978742913d53a6a
benoitmislin <benoit.mislin@gmail.com>
2023-08-10 11:31:27 +0200

    Suppression du dossier mairie68

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+    //pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
+  ]
+})
